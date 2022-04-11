1385e97570870a96b31da35df97c948b58b93e23
Eric Martínez <62176881+eric-mg-2000@users.noreply.github.com>
2022-04-11 19:57:10 +0200

    Initial setup
    Created initial setup

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.InputSystem;
+using System;
+
+public class Bullet : NetworkBehaviour
+{
+    #region Variables
+
+    #endregion
+
+    #region NetcodeVariables
+
+    #endregion
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.InputSystem;
+using System;
+
+public class Shooter : NetworkBehaviour
+{
+    #region Variables
+
+    #endregion
+
+    #region NetcodeVariables
+
+    #endregion
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
