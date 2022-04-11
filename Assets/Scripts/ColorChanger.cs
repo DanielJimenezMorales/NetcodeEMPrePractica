6073f163e46fdb99adb1a303bd72ff8e94f81e92
Daniel Jiménez Morales <64079694+DanielJimenezMorales@users.noreply.github.com>
2022-04-11 13:35:00 +0200

    Aniadida funcionalidad de cambiar color (Posee un pequenio bug al unirse un cliente)

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.Netcode;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class ColorChanger : NetworkBehaviour
+{
+    #region Variables
+    [SerializeField] private InputAction ColorChangeInput;
+    [SerializeField] private Color color1;
+    [SerializeField] private Color color2;
+    private Material playerMaterial = null;
+    #endregion
+
+    #region Netcode Variables
+    private NetworkVariable<Color> currentColor = new NetworkVariable<Color>(Color.white, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    #endregion
+
+    private void Awake()
+    {
+        playerMaterial = GetComponent<MeshRenderer>().material;
+    }
+
+    private void Start()
+    {
+        if (IsOwner)
+        {
+            ChangeColorServerRpc(color1);
+        }
+    }
+
+    private void OnEnable()
+    {
+        currentColor.OnValueChanged += UpdateColor;
+        ColorChangeInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        currentColor.OnValueChanged -= UpdateColor;
+        ColorChangeInput.Disable();
+    }
+
+    private void Update()
+    {
+        if(IsOwner)
+        {
+            if(ColorChangeInput.WasPressedThisFrame())
+            {
+                Color newColor = color1;
+
+                if(currentColor.Value == color1)
+                {
+                    newColor = color2;
+                }
+
+                ChangeColorServerRpc(newColor);
+            }
+        }
+    }
+
+    private void UpdateColor(Color previous, Color current)
+    {
+        playerMaterial.color = current;
+    }
+    
+    [ServerRpc]
+    private void ChangeColorServerRpc(Color newColor)
+    {
+        currentColor.Value = newColor;
+    }
+}
